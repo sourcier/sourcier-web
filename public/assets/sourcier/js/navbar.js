b8cdcc778177cd694343ffa2231af526732394a0
Roger Rajaratnam <roger@sourcier.uk>
2016-01-26 15:22:08 +0000

    Add navbar

---
+++ public/assets/sourcier/js/navbar.js
@@ -0,0 +1,28 @@
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 40) {
+                $('.navbar')
+                .removeClass('animated fadeOutUp')
+                .addClass('animated fadeInDown')
+                .fadeIn();
+                
+            } else {
+                $('.navbar')
+                .removeClass('animated fadeInDown')
+                .addClass('animated fadeOutUp')
+                .fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
