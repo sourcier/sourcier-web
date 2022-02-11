a9814f1774f510bd62286b16d978033280d7c33d
Roger Rajaratnam <roger@sourcier.uk>
2022-02-11 11:40:01 +0000

    chore: create ui library

---
+++ libs/ui/src/lib/hero/hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* eslint-disable-next-line */
+export interface HeroProps {}
+
+export function Hero(props: HeroProps) {
+  return (
+    <div>
+      <h1>Welcome to Hero!</h1>
+    </div>
+  );
+}
+
+export default Hero;
