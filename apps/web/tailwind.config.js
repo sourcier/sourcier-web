72384ed56f3fc9eedd2d8f0ffee612faae4ca841
Roger Rajaratnam <roger@sourcier.uk>
2022-02-16 21:31:42 +0000

    chore(web): update tailwind config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 module.exports = {
   presets: [require(join(__dirname, '../../tailwind.preset.js'))],
   content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,tsx}'),
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,tsx,js,jsx,mdx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
 };
