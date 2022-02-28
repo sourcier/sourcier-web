9bc3cc6875e24515f5559d42667b47d3f8af36a9
Roger Rajaratnam <roger@sourcier.uk>
2022-02-28 17:22:00 +0000

    chore: setup darkmode in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-postcss'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
+    'storybook-dark-mode',
+  ],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
--- libs/ui/.storybook/preview.js
+++ libs/ui/.storybook/preview.js
@@ -1,1 +1,8 @@
 import './main.scss';
+
+export const parameters = {
+  darkMode: {
+    darkClass: 'dark',
+    stylePreview: true,
+  },
+};
