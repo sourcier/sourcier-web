74a79c29e6a899845e40959ccb9143f6fae9e21f
Roger Rajaratnam <roger@sourcier.uk>
2022-03-11 00:37:49 +0000

    test(ui-e2e): fix hero e2e test

--- apps/ui-e2e/src/integration/hero/hero.spec.ts
+++ apps/ui-e2e/src/integration/hero/hero.spec.ts
@@ -1,7 +1,10 @@
 describe('ui: Hero component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=hero--primary'));
-    
+
     it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to Hero!');
+      cy.get('[data-testid="hero-heading"]').should(
+        'contain',
+        'Anim aute id magna'
+      );
     });
 });
