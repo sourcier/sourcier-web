5b9d094af7711f6f56d761e790f16de013162abd
Roger Rajaratnam <roger@sourcier.uk>
2022-03-11 21:58:53 +0000

    refactor(infrastructure): rename infrastructure -> sst

---
+++ libs/sst/README.md
@@ -0,0 +1,7 @@
+# sst
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test sst` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/sst/jest.config.js
+++ libs/sst/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'infrastructure',
+  displayName: 'sst',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/infrastructure',
+  coverageDirectory: '../../coverage/libs/sst',
 };
