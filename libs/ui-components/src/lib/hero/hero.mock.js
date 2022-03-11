a6542ee088cefd5f2a33998425df0ad4bb7a5f85
Roger Rajaratnam <roger@sourcier.uk>
2022-03-11 22:18:17 +0000

    chore(ui-components): remove existing components

--- libs/ui-components/src/lib/hero/hero.mock.js
+++
@@ -1,9 +0,0 @@
-export default {
-  image: {
-    src: 'https://source.unsplash.com/random',
-    alt: 'Random image',
-  },
-  heading: 'Anim aute id magna',
-  subHeading: 'aliqua ad deserunt',
-  copy: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat',
-};
