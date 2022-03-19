ad1f386b62bb56b20f752843b93f884b0d29ac06
Roger Rajaratnam <roger@sourcier.uk>
2022-03-19 03:01:31 +0000

    chore(sst): add custom domain cert config

--- libs/sst/src/lib/GatsbyStack.ts
+++ libs/sst/src/lib/GatsbyStack.ts
@@ -1,4 +1,5 @@
 import * as sst from '@serverless-stack/resources';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export default class GatsbyStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
@@ -12,6 +13,11 @@ export default class GatsbyStack extends sst.Stack {
         domainName:
           scope.stage === 'main' ? 'sourcier.uk' : `${scope.stage}.sourcier.uk`,
         domainAlias: scope.stage === 'prod' ? 'www.sourcier.uk' : undefined,
+        certificate: Certificate.fromCertificateArn(
+          this,
+          'SourcierUkCert',
+          'arn:aws:acm:eu-west-2:561381207426:certificate/d7f51f72-1e1c-4cc8-8631-3525a9aeddb2'
+        ),
       },
     });
 
