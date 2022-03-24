e150ab70ec8a77967c22dbdba1be44c5a846a567
Roger Rajaratnam <roger@sourcier.uk>
2022-03-24 14:16:37 +0000

    feat(web): add diff highlighting to code blocks

--- apps/web/src/components/code.tsx
+++ apps/web/src/components/code.tsx
@@ -3,6 +3,7 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/dracula';
 import rangeParser from 'parse-numeric-range';
 import clsx from 'clsx';
+import { BsClipboard, BsClipboardCheck } from 'react-icons/bs';
 
 const calculateLinesToHighlight = (raw) => {
   const lineNumbers = rangeParser(raw);
@@ -35,10 +36,10 @@ const Code = (props) => {
   );
 
   return (
-    <div className="bg-[#282a36] rounded-lg my-8 pl-6">
-      <div className="relative flex">
-        <div className="flex items-center justify-center px-2 mr-4 font-bold text-white uppercase rounded-b-lg bg-primary">{`${language}`}</div>
-        <div className="flex items-center justify-center font-serif italic text-gray-400">
+    <div className="bg-[#282a36] rounded-lg my-8">
+      <div className="relative flex px-4">
+        <div className="flex items-center justify-center px-2 font-bold text-white uppercase rounded-b-lg bg-primary">{`${language}`}</div>
+        <div className="flex items-center justify-center px-4 font-serif italic text-gray-400">
           {file && `${file}`}
         </div>
         <div className="flex-1" />
@@ -48,9 +49,14 @@ const Code = (props) => {
             setIsCopied(true);
             setTimeout(() => setIsCopied(false), 1000);
           }}
-          className="m-2 btn btn-primary btn-sm"
+          className="mt-3 btn btn-primary btn-sm"
         >
-          {isCopied ? '🎉 Copied!' : 'Copy'}
+          {isCopied ? (
+            <BsClipboardCheck className="mr-2" />
+          ) : (
+            <BsClipboard className="mr-2" />
+          )}
+          {isCopied ? 'Copied!' : 'Copy'}
         </button>
       </div>
       <div className="overflow-auto rounded-lg">
@@ -61,20 +67,29 @@ const Code = (props) => {
           theme={theme}
         >
           {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={clsx(className, 'm-0 px-0')} style={style}>
-              {tokens.map((line, i) => (
-                <div
-                  {...getLineProps({ line, key: i })}
-                  style={{
-                    background: highlights(i) ? '#44475a' : 'transparent',
-                  }}
-                  className="block"
-                >
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              ))}
+            <pre className={clsx(className, 'px-0 my-0')} style={style}>
+              {tokens.map((line, i) => {
+                // convert tokens to raw code string for setting diff highlight background
+                const text: string = line.reduce(
+                  (pv, cv) => (pv += cv.content),
+                  ''
+                );
+
+                return (
+                  <div
+                    {...getLineProps({ line, key: i })}
+                    className={clsx('block px-8', {
+                      'bg-[#44475a]': highlights(i),
+                      'bg-[#ff555580]': text.startsWith('-'),
+                      'bg-[#50fa7b33]': text.startsWith('+'),
+                    })}
+                  >
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                );
+              })}
             </pre>
           )}
         </Highlight>
