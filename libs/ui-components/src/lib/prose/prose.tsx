e486404ddd7b56239dc191ef48b7f7d9c2b677bd
Roger Rajaratnam <roger@sourcier.uk>
2022-04-07 00:49:49 +0100

    chore(ui-components): update prose link style

--- libs/ui-components/src/lib/prose/prose.tsx
+++ libs/ui-components/src/lib/prose/prose.tsx
@@ -4,7 +4,7 @@ export interface ProseProps {
 
 export function Prose({ children }: ProseProps) {
   return (
-    <div className="my-8 prose prose-lg max-w-none dark:prose-invert prose-img:rounded-lg prose-a:link-primary dark:prose-a:link-accent prose-headings:capitalize">
+    <div className="my-8 prose prose-lg max-w-none dark:prose-invert prose-img:rounded-lg prose-a:link-primary prose-a:link-hover dark:prose-a:link-accent prose-headings:capitalize">
       {children}
     </div>
   );
