a9de24675e1afd26e6a16ca17f4bcdbb19e27d9b
Roger Rajaratnam <roger@sourcier.uk>
2022-05-04 14:05:36 +0100

    chore: update nx to v14

--- libs/ui-components/jest.config.ts
+++ libs/ui-components/jest.config.ts
@@ -1,9 +1,10 @@
 module.exports = {
   displayName: 'ui-components',
-  preset: '../../jest.preset.js',
+
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/ui-components',
+  preset: '../../jest.preset.ts',
 };
